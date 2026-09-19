44d37d95f72b19e6d5f4dd9601025d9ce0af4f07
Alia-k-05 <lia.khan.2005@gmail.com>
2026-09-19 00:15:49 -0400

    Completed Notebook

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 # Gender -> 1 Female  0 Male
 # Churn -> 1 Yes  0 No
 # scaler is exported as scaler.pkl
+# Model is exported as model.pkl
+# Order of X -> is 'Age', 'Gender', 'Tenure', 'MonthlyCharges'
